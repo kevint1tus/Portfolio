c64280c0ee27be86b0c714cd675f38f9b51ac5c1
Kevin Titus <kxt309@student.bham.ac.uk>
2024-01-04 18:26:45 +0000

    Updates

--- next.config.js
+++ next.config.js
@@ -2,11 +2,20 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    reactStrictMode: true,
-}
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.(xml)$/,
+        use: ['xml-loader'],
+      });
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
