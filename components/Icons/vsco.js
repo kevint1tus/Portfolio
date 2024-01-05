9aef135101a058146b6f99513c005036b70b85fa
Kevin Titus <kxt309@student.bham.ac.uk>
2024-01-05 23:11:23 +0000

    Update vsco.js

--- components/Icons/vsco.js
+++ components/Icons/vsco.js
@@ -4,7 +4,11 @@ const IconVSCO = () => (
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      fill="#000000"
+      fill="none"
+      stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
       role="img"
       className="feather feather-vsco"
     >
