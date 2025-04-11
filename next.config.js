3bc0e8dd2e43c7525c7f12f51e9df1198d575269
kxt309 <kxt309@student.bham.ac.uk>
2025-04-11 15:25:40 +0100

    Remove unoptimized image setting and adjust build script in configuration files

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
